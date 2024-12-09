c090ee4ab751c0495a2cc46674b848f59287a358
DaaNMSR <denispresent4@gmail.com>
2024-12-09 22:18:23 +0300

    almost reade, but need some work(as modal and adaptation)

--- js/script.js
+++ js/script.js
@@ -27,3 +27,69 @@ tabsBtn.forEach((item) => {
         }
     })
 });
+
+const firstSlider = document.getElementById('first-slider');
+const secondSlider = document.getElementById('second-slider');
+const prevBtn = document.getElementById('prev-btn');
+const nextBtn = document.getElementById('next-btn');
+const reviewIndex = document.getElementById('review-index');
+
+prevBtn.addEventListener('click', () => {
+    let indexFirst = '1/2';
+
+    reviewIndex.innerHTML = indexFirst;
+
+    nextBtn.classList.remove('btn-opacity');
+    prevBtn.classList.add('btn-opacity');
+
+    firstSlider.style.display = 'flex';
+    secondSlider.style.display = 'none';
+})
+   
+nextBtn.addEventListener('click', () => {
+    let indexSecond = '2/2';
+
+    reviewIndex.innerHTML = indexSecond;
+
+    nextBtn.classList.add('btn-opacity');
+    prevBtn.classList.remove('btn-opacity');
+
+    firstSlider.style.display = 'none';
+    secondSlider.style.display = 'flex';
+})
+
+const openPopUp = document.querySelectorAll('.open__popup');
+const closePopUp = document.getElementById('close__popup');
+const popUp = document.getElementById('form-modal');
+
+openPopUp.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        popUp.classList.add('open-modal')
+    })
+})
+
+
+const openedPopUp = popUp.classList.contains('open-modal');
+console.log(openedPopUp);
+
+document.addEventListener('click', (e) => {
+        const clickPopUp = e.composedPath().includes(popUp);
+        if (clickPopUp) {
+            popUp.classList.remove('open-modal')
+        }
+})
+
+closePopUp.addEventListener('click', () => {
+    popUp.classList.remove('open-modal')
+})
+
+
+
+
+    
+
+
+
+
+
+
