6869f1c62573673f18a2710c5c135c2d6a1125ea
DaaNMSR <denispresent4@gmail.com>
2025-01-05 10:44:01 +0300

    update slider

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ const burgerButton = document.getElementById("burger");
 const body = document.getElementById("body");
 
 const openAndCloseBurgerMenu = () => {
-    headerContent.classList.toggle("open");
-    burgerButton.classList.toggle("open");
-    body.classList.toggle("scroll-off");
+  headerContent.classList.toggle("open");
+  burgerButton.classList.toggle("open");
+  body.classList.toggle("scroll-off");
 };
 
 burgerButton.addEventListener("click", openAndCloseBurgerMenu);
@@ -19,15 +19,15 @@ burgerButton.addEventListener("click", openAndCloseBurgerMenu);
 const cards = document.querySelectorAll(".first-screen__card");
 
 cards.forEach((card) => {
-    card.addEventListener("click", () => {
-        if (!card.classList.contains("active")) {
-            cards.forEach((card) => {
-                card.classList.remove("active");
-            });
-        }
+  card.addEventListener("click", () => {
+    if (!card.classList.contains("active")) {
+      cards.forEach((card) => {
+        card.classList.remove("active");
+      });
+    }
 
-        card.classList.add("active");
-    });
+    card.classList.add("active");
+  });
 });
 
 /////////////////////// first-screen cards END ////////////////////
@@ -39,33 +39,33 @@ const tabsButtons = document.querySelectorAll(".tabs__nav-btn");
 const tabsImages = document.querySelectorAll(".about-content-img");
 
 tabsButtons.forEach((currentButton) => {
-    const tabId = currentButton.getAttribute("data-tab");
-    const currentTab = document.querySelector(tabId);
+  const tabId = currentButton.getAttribute("data-tab");
+  const currentTab = document.querySelector(tabId);
 
-    const tabImgId = currentButton.getAttribute("data-img");
-    const currentImg = document.querySelector(tabImgId);
+  const tabImgId = currentButton.getAttribute("data-img");
+  const currentImg = document.querySelector(tabImgId);
 
-    const changeTabContent = () => {
-        if (!currentButton.classList.contains("active")) {
-            tabsButtons.forEach((btn) => {
-                btn.classList.remove("active");
-            });
+  const changeTabContent = () => {
+    if (!currentButton.classList.contains("active")) {
+      tabsButtons.forEach((btn) => {
+        btn.classList.remove("active");
+      });
 
-            tabs.forEach((tab) => {
-                tab.classList.remove("active");
-            });
+      tabs.forEach((tab) => {
+        tab.classList.remove("active");
+      });
 
-            tabsImages.forEach((image) => {
-                image.classList.remove("active");
-            });
+      tabsImages.forEach((image) => {
+        image.classList.remove("active");
+      });
 
-            currentButton.classList.add("active");
-            currentTab.classList.add("active");
-            currentImg.classList.add("active");
-        }
-    };
+      currentButton.classList.add("active");
+      currentTab.classList.add("active");
+      currentImg.classList.add("active");
+    }
+  };
 
-    currentButton.addEventListener("click", changeTabContent);
+  currentButton.addEventListener("click", changeTabContent);
 });
 //////////////////////////// tabs END //////////////////////////////
 
@@ -74,7 +74,7 @@ tabsButtons.forEach((currentButton) => {
 const slider = document.querySelector(".slider");
 const buttons = document.querySelectorAll(".slider__button");
 const items = document.querySelectorAll(".slider__item");
-const sliderItemWidth = document.querySelector(".slider__item").offsetWidth;
+let sliderItemWidth = document.querySelector(".slider__item").offsetWidth;
 const buttonSliderPrev = document.getElementById("slider__button-prev");
 const buttonSliderNext = document.getElementById("slider__button-next");
 const lastSlide = items.length;
@@ -82,52 +82,53 @@ let currentSlide = 1;
 let slidesToShow;
 
 const checkSlider = () => {
-    slider.clientWidth >= 1276 ? (slidesToShow = 3) : (slidesToShow = 1);
+  sliderItemWidth = document.querySelector(".slider__item").offsetWidth;
+  slider.clientWidth >= 1276 ? (slidesToShow = 3) : (slidesToShow = 1);
+  checkButtons();
 };
-
-checkSlider();
-
-// window.addEventListener("resize", checkSlider);
-
-buttons.forEach((button) => {
-    const scrollingSlide = () => {
-        slider.scrollLeft +=
-            button.id === "slider__button-prev"
-                ? -sliderItemWidth * slidesToShow
-                : sliderItemWidth * slidesToShow;
-    };
-
-    button.addEventListener("click", scrollingSlide);
-});
 
 const checkButtons = () => {
-    currentSlide === 1
-        ? (buttonSliderPrev.disabled = true)
-        : (buttonSliderPrev.disabled = false);
+  currentSlide === 1
+    ? (buttonSliderPrev.disabled = true)
+    : (buttonSliderPrev.disabled = false);
 
-    currentSlide === lastSlide / slidesToShow
-        ? (buttonSliderNext.disabled = true)
-        : (buttonSliderNext.disabled = false);
+  currentSlide === lastSlide / slidesToShow
+    ? (buttonSliderNext.disabled = true)
+    : (buttonSliderNext.disabled = false);
 };
 
-checkButtons();
+buttons.forEach((button) => {
+  const scrollingSlide = () => {
+    slider.scrollLeft +=
+      button.id === "slider__button-prev"
+        ? -sliderItemWidth * slidesToShow
+        : sliderItemWidth * slidesToShow;
+  };
+
+  button.addEventListener("click", scrollingSlide);
+});
 
 const changeCounterPrev = () => {
-    if (currentSlide > 1) {
-        currentSlide -= 1;
-        checkButtons();
-    }
+  if (currentSlide > 1) {
+    currentSlide -= 1;
+    checkButtons();
+  }
 };
 
 const changeCounterNext = () => {
-    if (currentSlide < lastSlide / slidesToShow) {
-        currentSlide += 1;
-        checkButtons();
-    }
+  if (currentSlide < lastSlide / slidesToShow) {
+    currentSlide += 1;
+    checkButtons();
+  }
 };
 
 buttonSliderPrev.addEventListener("click", changeCounterPrev);
 buttonSliderNext.addEventListener("click", changeCounterNext);
+
+window.addEventListener("resize", checkSlider);
+
+checkSlider();
+checkButtons();
 /////////////////////// slider END //////////////////////////
 
 /////////////////////// popup START /////////////////////////
@@ -137,31 +138,31 @@ const openPopUpButtons = document.querySelectorAll(".open__popup");
 const closePopUpButton = document.getElementById("close__popup");
 
 openPopUpButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-        popUp.classList.add("open-modal");
-        body.classList.add("scroll-off-modal");
-    });
+  button.addEventListener("click", () => {
+    popUp.classList.add("open-modal");
+    body.classList.add("scroll-off-modal");
+  });
 
-    closePopUpButton.addEventListener("click", () => {
-        popUp.classList.remove("open-modal");
-        body.classList.remove("scroll-off-modal");
-    });
+  closePopUpButton.addEventListener("click", () => {
+    popUp.classList.remove("open-modal");
+    body.classList.remove("scroll-off-modal");
+  });
 });
 
 const closePopUpOutClick = (e) => {
-    const target = e.target;
+  const target = e.target;
 
-    if (target === popUp) {
-        popUp.classList.remove("open-modal");
-        body.classList.remove("scroll-off-modal");
-    }
+  if (target === popUp) {
+    popUp.classList.remove("open-modal");
+    body.classList.remove("scroll-off-modal");
+  }
 };
 
 const closePopUpEsc = (e) => {
-    if (e.keyCode === 27) {
-        popUp.classList.remove("open-modal");
-        body.classList.remove("scroll-off-modal");
-    }
+  if (e.keyCode === 27) {
+    popUp.classList.remove("open-modal");
+    body.classList.remove("scroll-off-modal");
+  }
 };
 
 popUp.addEventListener("click", closePopUpOutClick);
